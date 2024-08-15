86f2919a21de96365d75beb71d3ec78dba665244
Anika <anika.tasnim0302@gmail.com>
2024-08-15 17:02:46 +0600

    DoctorAppointment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import signupRoute from "./routes/auth.js";
 import editProfileRoute from "./routes/editProfile.js";
 import resetpasswordRoute from "./routes/resetPass.js";
 import appointment from "./routes/appointment.js";
-
+import DoctorAppointment from "./routes/DoctorAppointment.js";
 
 dotenv.config();
 const app = express();
@@ -17,6 +17,7 @@ app.use("/auth", signupRoute);
 app.use("/auth", resetpasswordRoute);
 app.use("/edit", editProfileRoute);
 app.use("/api", appointment);
+app.use("/", DoctorAppointment);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
